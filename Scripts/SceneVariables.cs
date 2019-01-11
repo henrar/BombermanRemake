177430efaacbed377f126f7b7deef5e42065c3ab
Bartłomiej Kumor <5493873+henrar@users.noreply.github.com>
2019-01-11 17:34:59 +0100

    Forgotten file

--- Scripts/SceneVariables.cs
+++ Scripts/SceneVariables.cs
@@ -1,12 +1,25 @@
 using Godot;
 using System;
 
+public struct EnemyCountConfig {
+    public int GetEnemyCount() {
+        return this.numberOfBalloons + this.numberOfShrooms + this.numberOfBarrels; 
+    }
+
+    public int numberOfBalloons;
+    public int numberOfShrooms;
+    public int numberOfBarrels;
+}
+
 public class SceneVariables : Node {
     public Node CurrentScene { get; set; }
     public int numberOfLives;
     public int playerMovementSpeed;
 
-    public int numberOfEnemies;
+    EnemyCountConfig level1Config;
+    EnemyCountConfig level2Config;
+    EnemyCountConfig level3Config;
+
     public int numberOfPowerUps;
     public float powerup1DropChance;
     public float powerup2DropChance;
@@ -26,11 +39,24 @@ public class SceneVariables : Node {
     public bool godMode;
 
     public int coinCountToSpawn;
+    public int ghostCountToSpawnOnExitExplosion;
 
     public override void _Ready() {
         Viewport root = GetTree().GetRoot();
         this.CurrentScene = root.GetChild(root.GetChildCount() - 1);
-        this.numberOfEnemies = 6; //TODO: change on level transitions
+
+        level1Config.numberOfBalloons = 6;
+        level1Config.numberOfShrooms = 0;
+        level1Config.numberOfBarrels = 0;
+
+        level2Config.numberOfBalloons = 4;
+        level2Config.numberOfShrooms = 3;
+        level2Config.numberOfBarrels = 0;
+
+        level3Config.numberOfBalloons = 3;
+        level3Config.numberOfShrooms = 3;
+        level3Config.numberOfBarrels = 3;
+
         this.numberOfPowerUps = 3;
 
         this.currentLevel = 1; 
@@ -72,12 +98,28 @@ public class SceneVariables : Node {
         this.numberOfLives = 3;
 
         this.coinCountToSpawn = 30;
+        this.ghostCountToSpawnOnExitExplosion = 3;
 
         //CHEATS
         this.aiDisabled = false;
         this.godMode = false;
+    }
 
-        Console.WriteLine("Ready SV");
+    public EnemyCountConfig GetEnemyConfig() {
+        switch(this.currentLevel) {
+            case 1: {
+                    return this.level1Config;
+                }
+            case 2: {
+                    return this.level2Config;
+                }
+            case 3: {
+                    return this.level3Config;
+                }
+            default: {
+                    return this.level1Config;
+                }
+        }
     }
 
     public void ResetPlayerVariablesOnDeath() {
